98b1fc87b37de9e6346b21f224f9fe529772ce29
zhangruida <zhangruida@bytedance.com>
2018-08-03 10:03:14 +0800

    主体结构

--- socket/main.go
+++ socket/main.go
@@ -1,5 +1,61 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+const (
+	SERVER_NETWORK = "tcp"
+	SERVER_ADDRESS = "127.0.0.1:8085"
+	DELIMITER      = "\t"
+)
+
+func printLog(role string, sn int, format string, args ...interface{}) {
+	if !strings.HasSuffix(format, "\n") {
+		format += "\n"
+	}
+	fmt.Printf("%s[%d]: %s", role, sn, fmt.Sprintf(format, args...))
+}
+
+func printServerLog(format string, args ...interface{}) {
+	printLog("Server", 0, format, args...)
+}
+
+func printClientLog(sn int, format string, args ...interface{}) {
+	printLog("Client", sn, format, args...)
+}
+
+func handleConn(conn net.Conn) {
+
+}
+
+func serverGo() {
+	var listener net.Listener
+	listener, err := net.Listen(SERVER_NETWORK, SERVER_ADDRESS)
+	if err != nil {
+		printServerLog("Listen Error: %s", err)
+		return
+	}
+	defer listener.Close()
+	printServerLog("Got listener for the server. (local address: %s)", listener.Addr())
+
+	for {
+		// 阻塞直至新链接到来
+		conn, err := listener.Accept()
+		if err != nil {
+			printServerLog("Accept Error: %s", err)
+		}
+		printServerLog("Established a connection with a client application. (remote address: %s)", conn.RemoteAddr())
+		go handleConn(conn)
+	}
+}
+
+func clientGo() {
+
+}
+
 func main() {
 
 }
